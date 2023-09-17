3c83f8ed5be08df96c1dd10525d5186e49b676ae
Lucas de Paiva Ribeiro <proftilucas@outlook.com>
2023-09-17 20:56:55 -0300

    Desktop

--- contosoFaturas/bot.py
+++ contosoFaturas/bot.py
@@ -1,68 +1,71 @@
 from botcity.core import DesktopBot
+import pandas as pd
 
 # from botcity.maestro import *
 
 def not_found(label):
     print(f"Element not found: {label}")
 
-def cadastraFaturas():
-    bot = DesktopBot()
-    path_app = r'C:\Program Files (x86)\Contoso, Inc\Contoso Invoicing\LegacyInvoicingApp.exe'
+dados = pd.read_excel('C:/Users/lucas/developer/back-end/rpa_python/contosoFaturas/file/Contoso Coffee Shop Invoices.xlsx')
+print(dados)
 
-    bot.execute(path_app)
-    bot.wait(1500)
-    bot.maximize_window()
-    bot.wait(1000)
-    
-    
-    if not bot.find( "invoices", matching=0.97, waiting_time=10000):
-        not_found("invoices")
-    bot.click()
-    
+bot = DesktopBot()
+path_app = 'C:/Program Files (x86)/Contoso, Inc/Contoso Invoicing/LegacyInvoicingApp.exe'
+
+bot.execute(path_app)
+bot.wait(1500)
+bot.maximize_window()
+bot.wait(1000)
+
+if not bot.find( "invoices", matching=0.97, waiting_time=10000):
+    not_found("invoices")
+bot.click()
+
+def cadastraFaturas(data, conta, contato, valor, status):
     if not bot.find( "novo_registro", matching=0.97, waiting_time=10000):
         not_found("novo_registro")
     bot.click()
-        
-        
-        
-        
+               
     if not bot.find( "date", matching=0.97, waiting_time=10000):
         not_found("date")
     bot.click_relative(71, 8)
     bot.type_keys(['home'])
     bot.type_keys(['shift', 'end'])
-    bot.paste('09/15/2023')
+    bot.paste(data)
 
     bot.tab()
-    bot.paste('123456')
+    bot.paste(conta)
     bot.tab()
-    bot.paste('bot@gmail.com')
+    bot.paste(contato)
     bot.tab()
-    bot.paste('10000')
+    bot.paste(valor)
     
+    coluna = status
+
     if not bot.find( "status-inicio", matching=0.97, waiting_time=10000):
         not_found("status-inicio")
     bot.click_relative(56, 7)
        
-    if not bot.find( "univoiced", matching=0.97, waiting_time=10000):
-        not_found("univoiced")
-    bot.click_relative(62, 32)
+    if coluna == "Uninvoiced":
+        if not bot.find( "univoiced", matching=0.97, waiting_time=10000):
+            not_found("univoiced")
+        bot.click_relative(62, 32)
+
+    elif coluna == "Invoiced":
+        if not bot.find( "invoiced", matching=0.97, waiting_time=10000):
+            not_found("invoiced")
+        bot.click_relative(62, 56)
     
-    if not bot.find( "invoiced", matching=0.97, waiting_time=10000):
-        not_found("invoiced")
-    bot.click_relative(62, 56)
+    else:
+        if not bot.find( "paid", matching=0.97, waiting_time=10000):
+            not_found("paid")
+        bot.click_relative(60, 76)
+        
+    if not bot.find( "salvar", matching=0.97, waiting_time=10000):
+        not_found("salvar")
+    bot.click()
     
-    if not bot.find( "paid", matching=0.97, waiting_time=10000):
-        not_found("paid")
-    bot.click_relative(60, 76)
-    
-    
-    
-cadastraFaturas()
+for coluna in dados.itertuples():
+    cadastraFaturas(str(coluna[1]), str(coluna[2]), str(coluna[3]), str(coluna[4]), str(coluna[5]))
 
-
-
-
-
-
-
+bot.alt_f4()
